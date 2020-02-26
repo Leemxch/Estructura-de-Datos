1c35ae89ff1d6a498eb1df6d35a6890fb5e382aa
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-26 15:43:51 -0600

    Update Ejercicio1-19.c

--- Tarea1/Ejercicio1-19.c
+++ Tarea1/Ejercicio1-19.c
@@ -1,1 +1,22 @@
-a
+#include<stdio.h>
+char reverse(char palabra[], int num){
+	for(int i=0; i<num; i++){
+		printf("%c", palabra[num-1-i]);
+	}
+	
+}
+void main() {
+	char palabra[100]="", c;
+	int count=0;
+	while ((c = getchar()) != EOF) {
+       		if (c == ' ' || c == '\t' || c == '\n') {
+            		reverse(palabra, count);
+			count = 0;
+			putchar(c);
+        	} 
+		else {
+			palabra[count]=c;
+            		count++;
+        	}
+    	}
+}
