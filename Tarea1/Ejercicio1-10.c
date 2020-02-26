189a4e804af15a634b03f58d4adf5b8d84fca9c4
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-26 00:08:50 -0600

    Update Ejercicio1-10.c

--- Tarea1/Ejercicio1-10.c
+++ Tarea1/Ejercicio1-10.c
@@ -1,1 +1,30 @@
-a
+#include <stdio.h>
+
+void main(){
+    int input, stop;
+    stop = 1;
+    while ((input = getchar()) != EOF) {
+        stop = 1;
+        if (input == '\t' ) {
+            putchar('\\');
+            putchar('t');
+            stop = 0;
+        }
+        if (input == ' ') {
+            putchar('\\');
+            putchar('b');
+            stop = 0;
+        }
+        if (input == '\\') {
+            putchar('\\');
+            putchar('\\');
+            stop = 0;
+        }
+        else {
+            if (stop == 1) {
+                printf("%c", input);
+            }
+        }
+ 
+    }
+}
