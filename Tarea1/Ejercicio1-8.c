063c37e4ea96fe1aa1c1e4fb00fe1334d6f02a19
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-25 23:20:13 -0600

    Update and rename Ejercicio1.8.c.txt to Ejercicio1-8.c

---
+++ Tarea1/Ejercicio1-8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* Cuenta los caracteres y saltos de linea de la entrada */
+void main(){
+    int input, count;
+    count = 0;
+    while ((input = getchar()) != EOF) {
+        if (input == '\n')
+            ++count;
+        else(++count);
+        printf("%d\n", count);
+    }      
+}
