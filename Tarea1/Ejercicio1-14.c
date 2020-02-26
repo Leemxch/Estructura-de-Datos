fe7f06090c229be89fc3515a713a1e2d04d3ee72
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-26 15:36:42 -0600

    Update Ejercicio1-14.c

--- Tarea1/Ejercicio1-14.c
+++ Tarea1/Ejercicio1-14.c
@@ -1,1 +1,23 @@
-a
+void main(){
+	int c, characters[94][2], dec;
+	for (int i=0; i<94;i++){
+		characters[i][0]=i+32;
+		characters[i][1]=0;
+	}
+	while ((c = getchar()) != EOF) {
+		dec = c;
+		dec -= 32;
+		if (dec>=0 && dec<=94){
+			characters[dec][1]++;
+		}
+	}
+	for (int i=0; i<94; i++){
+		if (characters[i][1] !=0 ){
+			printf("%c | ", characters[i][0]);
+			for (int ii=0; ii<characters[i][1];ii++){
+				printf("X");
+			}
+			printf("\n");
+		}
+	}
+}
