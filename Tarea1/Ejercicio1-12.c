6827efa6af423949ba57f2ca088daa61b05be2f5
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-26 00:16:10 -0600

    Update Ejercicio1-12.c

--- Tarea1/Ejercicio1-12.c
+++ Tarea1/Ejercicio1-12.c
@@ -1,1 +1,13 @@
-a
+#include <stdio.h>
+
+void main() {
+	int input;
+	while ((input = getchar()) != EOF) {
+		if (input == ' ') {
+			printf("\n");
+		}
+		else {
+			printf("%c", input);
+		}
+	}
+}
