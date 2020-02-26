18106b950987e9110dc8a27eb4731245bf8ce170
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-26 10:49:10 -0600

    Update Ejercicio1-15.c

--- Tarea1/Ejercicio1-15.c
+++ Tarea1/Ejercicio1-15.c
@@ -1,1 +1,17 @@
-a
+#include <stdio.h>
+int degree(int x);
+
+int main()
+{
+    int i;
+    for (i = 0; i < 300; i = i+20)
+        printf("%d\t%d \n", i, degree(i));
+    return 0;
+}
+
+int degree(int base)
+{
+    int ans;
+    ans = (5*(base-32))/9;
+    return ans;
+}
