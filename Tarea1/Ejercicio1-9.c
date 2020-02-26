32cc0781e0be33028f18a75d998625efba3794d9
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-25 23:37:15 -0600

    Update Ejercicio1-9.c

--- Tarea1/Ejercicio1-9.c
+++ Tarea1/Ejercicio1-9.c
@@ -1,1 +1,19 @@
-a
+#include <stdio.h>
+/* Reescribe la entrada con varios espacios en uno solo */
+void main(){
+    int input, count;
+    count = 0;
+    while ((input = getchar()) != EOF) {
+        if (input == ' ') {
+            if (count < 1) {
+                printf("%c", input);
+                ++count;
+            }
+
+        }
+        else {
+            printf("%c", input);
+                count = 0;
+        }
+    }
+}
