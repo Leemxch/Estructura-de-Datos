ee62894879907b44f21a0975744bb4481e14e487
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-26 13:34:31 -0600

    Update Ejercicio1-17.c

--- Tarea1/Ejercicio1-17.c
+++ Tarea1/Ejercicio1-17.c
@@ -4,17 +4,18 @@ int gline(char line[], int maxline);
 void copy(char to[], char from[]);
  
 void main(){
-  int len; 
+  	int len; 
  	int max; 
  	char line[MAXLINE]; 
  	char longest[MAXLINE]; 
- 	max = 80;
+ 	max = 0;
  	while ((len = gline(line, MAXLINE)) > 0)
  		if (len > max) {
  			max = len;
  			copy(longest, line);
  		}
  	if (max >= 80){
+		printf("%s\n", "La linea mas larga es:")
  		printf("%s", longest);
 	}
 	else{
