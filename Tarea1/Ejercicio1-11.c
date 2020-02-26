14c7478496711341ba74cb1abea9cadf3d5c7bb3
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-26 15:28:37 -0600

    Update Ejercicio1-11.c

--- Tarea1/Ejercicio1-11.c
+++ Tarea1/Ejercicio1-11.c
@@ -1,1 +1,23 @@
-a
+#include <stdio.h>
+
+void main(){
+    int c;
+    int before, numWords;
+	numWords = 0;
+
+    before = 1;
+    while ((c = getchar()) != EOF) {
+  		if (c == ' ' || c == '\t' || c == '\n') {
+       		if (before == 0) {
+         		before = 1;
+				numWords++;
+			}
+        } 
+		else {
+    		before = 0;
+    	}
+    }
+    if (before == 0){
+        numWords++;
+    }
+	printf("\nEl numero de palabras es: %d \n", numWords);
