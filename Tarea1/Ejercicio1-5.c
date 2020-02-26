0731c97f0c6a52cacd6188a3b0df181b406257e3
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-25 22:58:22 -0600

    Update Ejercicio1-5.c

--- Tarea1/Ejercicio1-5.c
+++ Tarea1/Ejercicio1-5.c
@@ -1,1 +1,16 @@
-a
+#include <stdio.h>
+/* print Fahrenheit-Celsius table for fahr = 300, 280, ..., 0 */
+void main(){
+    int fahr, celsius;
+    int lower, upper, step;
+    lower = 0; /* lower limit of temperature scale */
+    upper = 300; /* upper limit */
+    step = 20; /* step size */
+    fahr = upper;
+    printf("Fahrenheit\t\tCelsius\n");
+    while (fahr >= lower) {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%d\t\t\t%d\n", fahr, celsius);
+        fahr = fahr - step;
+    }
+}
