9e6f720b7825c27d922fcc70aa4b8444e61057b6
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-25 23:12:57 -0600

    Update Ejercicio1-6.c

--- Tarea1/Ejercicio1-6.c
+++ Tarea1/Ejercicio1-6.c
@@ -1,1 +1,13 @@
-a
+#include <stdio.h>
+/* copy input to output; 1st version */
+void main()
+{
+    int c, value;
+    c = getchar();
+    while (c != EOF) {
+        putchar(c);
+        c = getchar();
+        value = c != EOF;
+        printf("\n%d\n", value);
+    }
+}
