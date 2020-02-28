fbb784e9bb64d3551a2592fd7e26ce1782f22a70
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-27 21:13:59 -0600

    Update Ejercicio2-10.c

--- Tarea1/Ejercicio2-10.c
+++ Tarea1/Ejercicio2-10.c
@@ -1,1 +1,21 @@
-a
+#include <stdio.h>
+void lower(char string1[100]){
+	int i=0;
+	int tem;
+	char string2[100];
+	while (string1[i] != '\0'){
+		we = string1[i];
+		if (string1[i] <= 90 && string1[i]<=65){
+			tem = string1[i]+32;
+		}
+		string2[i]=tem;
+		i++;
+	}
+	printf("%s",string2);
+	putchar('\n');
+		
+}
+
+void main(){
+	lower("AAAa\0");
+}
