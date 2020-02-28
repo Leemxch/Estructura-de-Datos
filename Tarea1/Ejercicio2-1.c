f0718f86eba68bde86f1c4624adc225b1b83efb2
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-27 21:09:43 -0600

    Update Ejercicio2-1.c

--- Tarea1/Ejercicio2-1.c
+++ Tarea1/Ejercicio2-1.c
@@ -4,5 +4,5 @@ void main(){
 	printf("Maximo valor de un unsigned int = 4 294 967 296\n");
 	printf("Signed int = - 2 147 483 648\n\n");
 	printf("Maximo valor de un unsigned char = 255\n");
-	prinft("Signed char = -128");
+	printf("Signed char = -128");
 }
