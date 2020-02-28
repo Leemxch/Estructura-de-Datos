62fe53b924594ba9b46774931fdd9fb78f9ab104
Leemxch <61258635+Leemxch@users.noreply.github.com>
2020-02-27 21:12:59 -0600

    Update Ejercicio2-5.c

--- Tarea1/Ejercicio2-5.c
+++ Tarea1/Ejercicio2-5.c
@@ -1,1 +1,16 @@
-a
+#include <stdio.h>
+int any(char string1[], char string2[]){
+	int numero = 1;
+	for (int i=0;string1[i]!= '\0';i++){
+		for (int ii=0; string2[ii] !='\0';ii++){
+			if (string1[i] == string2[ii]){
+				return numero;
+			}
+		}
+		numero++;
+	}
+	return -1;
+}
+void main(){
+	printf("%d\n",any("Hola\0", "anda\0"));
+}
